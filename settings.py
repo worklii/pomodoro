fb671799f2617eb1fb9c30398b25420bc6fb5306
Nikita Li <nikitali@Mac.fritz.box>
2025-08-06 20:33:26 +0200

    fix database and cache accesor

--- settings.py
+++ settings.py
@@ -1,11 +1,16 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DB_HOST: str = '0.0.0.0'
+    DB_HOST: str = 'localhost'
     DB_PORT: int = 5432
     DB_USER: str = 'postgres'
     DB_PASSWORD: str = 'password'
     DB_NAME: str = 'pomodoro'
-    CACHE_HOST: str = '0.0.0.0'
+    CACHE_HOST: str = 'localhost'
     CACHE_PORT: int = 6379
     CACHE_DB: int = 0
+    DB_DRIVER: str = 'postgresql+psycopg2'
+
+    @property
+    def db_url(self):
+        return f'{self.DB_DRIVER}://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
